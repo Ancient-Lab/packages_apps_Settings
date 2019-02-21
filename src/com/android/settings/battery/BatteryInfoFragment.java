f9987ff4bad23a84ec1f1a313231b8032b0017f2
AnkitGourav <Ankitgourav035@gmail.com>
2019-02-21 21:13:43 +0530

    Advanced Battery Info Plus
    Extension of Havoc-OS Battery Info
    Thanks to @SKULSHADY for initial implementation
    
    @Aston-Martinn | Rebellion-OS Forward port to Q
    
    * Added Battery Level
    * Added Charging Type
    * Added Charge Holding Capacity
    * Added Current
    * Added Max. Charging Current
    * Added Voltage
    * Added Max. Voltage
    * Added Qcom Battery Version
    * Added System Temperature Level
    
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>
    Signed-off-by: smokey18 <kmboplays@gmail.com>

--- src/com/android/settings/battery/BatteryInfoFragment.java
+++ src/com/android/settings/battery/BatteryInfoFragment.java
@@ -27,7 +27,18 @@ import com.android.settings.battery.BatteryHealthPreferenceController;
 import com.android.settings.battery.BatteryStatusPreferenceController;
 import com.android.settings.battery.BatteryTechPreferenceController;
 import com.android.settings.battery.ChargeCyclePreferenceController;
+import com.android.settings.battery.CapacityPreferenceController;
 import com.android.settings.battery.ChargeFullDesignPreferenceController;
+import com.android.settings.battery.ChargeFullPreferenceController;
+import com.android.settings.battery.ChargeTypePreferenceController;
+import com.android.settings.battery.CurrentMaxPreferenceController;
+import com.android.settings.battery.CurrentPreferenceController;
+import com.android.settings.battery.OriginalBatteryCapacityPreferenceController;
+import com.android.settings.battery.QcomBatteryVersionPreferenceController;
+import com.android.settings.battery.SysTempLevelPreferenceController;
+import com.android.settings.battery.VoltageMaxPreferenceController;
+import com.android.settings.battery.VoltagePreferenceController;
+
 import com.android.settingslib.core.AbstractPreferenceController;
 
 import androidx.lifecycle.Lifecycle;
@@ -41,7 +52,7 @@ public class BatteryInfoFragment extends DashboardFragment {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.REBELLION_SETTINGS;
+        return MetricsEvent.ANCIENT_SETTINGS;
     }
 
     @Override
@@ -71,6 +82,16 @@ public class BatteryInfoFragment extends DashboardFragment {
         controllers.add(new BatteryTechPreferenceController(context));
         controllers.add(new ChargeCyclePreferenceController(context));
         controllers.add(new ChargeFullDesignPreferenceController(context));
+        controllers.add(new ChargeFullPreferenceController(context));
+        controllers.add(new ChargeTypePreferenceController(context));
+        controllers.add(new CapacityPreferenceController(context));
+        controllers.add(new CurrentMaxPreferenceController(context));
+        controllers.add(new CurrentPreferenceController(context));
+        controllers.add(new OriginalBatteryCapacityPreferenceController(context));
+        controllers.add(new QcomBatteryVersionPreferenceController(context));
+        controllers.add(new SysTempLevelPreferenceController(context));
+        controllers.add(new VoltageMaxPreferenceController(context));
+        controllers.add(new VoltagePreferenceController(context));
         return controllers;
     }
 
