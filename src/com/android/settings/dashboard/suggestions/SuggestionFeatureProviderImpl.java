6b20f5f39a9e485f9001e62555b65ef37448f6c5
Ali B <abittin@gmail.com>
2019-05-13 20:10:41 +0300

    Settings: remove LiveDisplay if not supported
    Change-Id: I0032b88e1fcaf918935b861434c8b4a4cf090cd9
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- src/com/android/settings/dashboard/suggestions/SuggestionFeatureProviderImpl.java
+++ src/com/android/settings/dashboard/suggestions/SuggestionFeatureProviderImpl.java
@@ -22,6 +22,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 import android.provider.Settings;
+import android.hardware.display.ColorDisplayManager;
 
 import androidx.annotation.NonNull;
 
@@ -78,7 +79,8 @@ public class SuggestionFeatureProviderImpl implements SuggestionFeatureProvider 
         } else if (className.equals(WifiCallingSuggestionActivity.class.getName())) {
             return WifiCallingSuggestionActivity.isSuggestionComplete(context);
         } else if (className.equals(NightDisplaySuggestionActivity.class.getName())) {
-            return NightDisplayPreferenceController.isSuggestionComplete(context);
+            return ColorDisplayManager.isNightDisplayAvailable(context) &&
+                NightDisplayPreferenceController.isSuggestionComplete(context);
         } else if (className.equals(ZenSuggestionActivity.class.getName())) {
             return ZenOnboardingActivity.isSuggestionComplete(context);
         }
